c03bb72220e8ed27fe547d80cf34587b253148ef
Mauro Ulloa <ulloamauro529@gmail.com>
2024-06-21 20:22:20 -0300

    Ejercicios de JAVASCRIPT

---
+++ Javascript/Leccion03/03-01-OperadoresJavaScript.js
@@ -0,0 +1,26 @@
+// Ejercicio para encontrar numeros pares
+let parInpar = 5;
+if(parInpar  % 2 == 0){
+    console.log("Es un numero PAR")
+}
+else{
+    console.log("Es un numero IMPAR")
+}
+//Ejercicio es mayor de edad
+var edad = 18
+if(edad >= 18){
+    console.log("Es MAYOR de edad")
+}
+else{
+    console.log("Es MENOR de edad")
+}
+// Ejercicio dentro de un rango
+let dentrorango = 5;
+let valMin = 0, valMax = 10;
+if(dentrorango >= valMin && dentrorango <= valMax){
+    console.log('Esta dentro del rango establecido')
+} 
+else{
+    console.log('Esta fuera del rango establecito')
+}
+
